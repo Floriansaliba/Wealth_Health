2b32bdee985420895ef80f3196ae93de87876424
floriansaliba <florian.saliba@gmail.com>
2022-06-27 17:41:27 +0200

    commit after presentation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,5 @@
 import Header from "../components/Header"
 import Form from "../components/Form"
-// import ModalAlert from "../components/ModalAlert"
-// import {Modal} from "react-modal-p14-openclassrooms"
-import ModalAlert from "../components/ModalAlert"
 import { useState } from "react"
 import { Modal } from "react-modal-p14-openclassrooms"
 
@@ -12,14 +9,11 @@ import { Modal } from "react-modal-p14-openclassrooms"
 const HomePage = ()=>{
 
 const [modal, setModal]= useState(false)
-
-
     return modal ? (
         <div>
         <Header />
         <Form />
         <Modal text = "Employee Created!"  closeModal={()=>{setModal(false)}}/>
-        {/* <ModalAlert text = "Employee Created!" closeModal={()=>{setModal(false)}} />  */}
         </div>
     ) : (
         <div>
